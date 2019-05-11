bb85f4af505428e8743ca194a298525b7c1a311f
kofimorrison <49091928+kofimorrison@users.noreply.github.com>
2019-05-11 23:57:36 +0200

    Updates

---
+++ precache-manifest.5fb68eb8004f784beaf89356e4afca20.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cd0feee87716ee5dd54d32d6126789b",
+    "url": "/Bildersuche-App/index.html"
+  },
+  {
+    "revision": "2300d708075d70876f27",
+    "url": "/Bildersuche-App/static/css/main.c00f04db.chunk.css"
+  },
+  {
+    "revision": "7e6ac1444ef9242ebc00",
+    "url": "/Bildersuche-App/static/js/2.824d5390.chunk.js"
+  },
+  {
+    "revision": "2300d708075d70876f27",
+    "url": "/Bildersuche-App/static/js/main.4ec956b9.chunk.js"
+  },
+  {
+    "revision": "ff135f491edb00f29fe4",
+    "url": "/Bildersuche-App/static/js/runtime~main.29d742ce.js"
+  }
+]);
